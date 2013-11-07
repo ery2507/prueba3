b273eac780461982afda0112ac49b8cd971ba21d
ERIKA CAMACHO MOTOLINIA <ery2507.ec@gmail.com>
2013-11-07 17:03:28 -0600

    prueba31
    vibrar

--- js/acciones.js
+++ js/acciones.js
@@ -3,6 +3,14 @@ $(document).ready(function(e){// e de evento
 	document.addEventListener("deviceready",function(){
 		$('.btn').tap(function(){
 			var boton =(($(this).attr('class')).split(' '))[1];
+			switch (boton){
+				case 'b1': 
+				navigator.notificacion.beep(2);
+				break;
+				case 'v1': 
+				navigator.notificacion.vibrate(1000);
+				break;
+				}
 		});//tap
 	},false);// deviceready
 });// ready
