1918a3e63ca66703e29089d71d78acb794ea9c41
ery2507 <ery2507.ec@gmail.com>
2013-11-12 17:06:11 -0600

    dfg

--- js/acciones.js
+++ js/acciones.js
@@ -1,32 +1,6 @@
 // JavaScript Document
 $(document).ready(function(e){// e de evento
 	document.addEventListener("deviceready",function(){
-		$('.btn').tap(function(){
-			var boton =(($(this).attr('class')).split(' '))[1];
-			switch (boton){
-				case 'b1': 
-				navigator.notification.beep(2);
-				break;
-				case 'v1': 
-				navigator.notification.vibrate(1000);
-				break;
-				}//switch
-			
-		});//tap
-		$('#izq').swipeleft(function (){
-				navigator.notification.alert("Deslizó a la izquierda",function(){"prueba4","Aceptar"});//alert
-			});//barrer a la izquierda
-			$('#der').swiperigth(function (){
-				navigator.notification.confirm("que deseas realizar",function(opt){
-				switch (opt){
-					case 1: 
-						navigator.notification.beep(4);
-					break;
-					case 2: 
-						navigator.notification.vibrate(2000);
-					break;
-				}//switch
-					}"Prueba04","Beep, Vibrar, Cancelar");//confirm
-			});//barrer a la derecha
+		navigator.notification.alert ("hola");
 	},false);// deviceready
 });// ready
